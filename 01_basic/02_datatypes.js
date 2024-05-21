f789194883da2ceea8469dc4cad85bbc47f20ede
Kalp830 <167209197+Kalp830@users.noreply.github.com>
2024-05-21 10:55:13 +0000

    data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => Standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
