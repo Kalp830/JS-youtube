f9286ea3d3cc12a29d70f0f9d0f2e89ef13e79d9
Kalp830 <167209197+Kalp830@users.noreply.github.com>
2024-05-22 11:12:07 +0000

    About String Learning

---
+++ 01_basic/05_string.js
@@ -0,0 +1,16 @@
+const name = "kalpesh"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('HelloWorld!')
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+console.log(gameName.indexOf('l'));
+console.log(gameName.charAt(3));
+console.log(gameName.repeat(1));
+
+const newString = gameName.substring(0, 6);
+console.log(newString);
+const anotherString = gameName.slice(0, 4);
+console.log(anotherString);
