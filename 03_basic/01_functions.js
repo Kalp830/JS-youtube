2a4f58d5790821ecfea4ded6f6b22455da5b0e20
Kalp830 <167209197+Kalp830@users.noreply.github.com>
2024-05-26 14:19:31 +0000

    function and scopes

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -13,4 +13,58 @@ multiNames();
 function myNumber(num1, num2){
     console.log(num1 + num2);
 }
-myNumber(12,2);
+// myNumber(12,2);
+
+
+function myNumber(num1, num2){
+    return num1 + num2
+}
+const result = myNumber(12,2)
+// console.log("Result", result);
+
+
+function userNameLogin(username){
+    return `Print the ${username} who want's to loin`;
+}
+// console.log(userNameLogin("Kalpesh"))
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+
+function calculateCartPrice(...number){
+    return number;
+}
+// console.log(calculateCartPrice(200, 331, 500,600));
+
+
+
+// Pass the object data through function.
+const user = {
+    userName: "Kalptaru",
+    age:36
+
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.userName} and price is ${anyObject.age}`);
+}
+
+handleObject(user);
+
+// Pass the array data through function.
+
+const myArry = [200,300,900,400];
+
+function returnArrayValue(getArry){
+    return getArry[2]
+}
+console.log(returnArrayValue(myArry));
+console.log(returnArrayValue([100,700,100,500]));
