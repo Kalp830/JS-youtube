603b9378aea21eaeadbc29e3a0622fb3fbc046cf
Kalpesh Talekar <167209197+Kalp830@users.noreply.github.com>
2024-05-28 13:16:19 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+// const temprature = 55;
+
+// if(temprature < 50){
+//      console.log("less than 50");
+// }else
+// console.log("temperature is greater than 50");
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 100;
+
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`User Power: ${power}`);
+// }
+const balance = 1000;
+
+if(balance < 500){
+    console.log("less than 500");
+}
+else if(balance < 750){
+    console.log("less than 750");
+}
+else if(balance > 750){
+    console.log("grater 750");
+}
+else{
+    console.log("less than 1000");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,39 @@
+// Switch Case
+let month = 4;
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+
+    default:
+        break;
+}
+
+let months = "mar";
+switch (months) {
+    case "jan":
+        console.log("Jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "mar":
+        console.log("mar");
+        break;
+    case "apr":
+        console.log("Apr");
+        break;
+
+    default:
+        break;
+}
